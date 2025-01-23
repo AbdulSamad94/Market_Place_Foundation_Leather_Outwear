732165e6218da606e20f391f0d0195993d75eb02
AbdulSamad94 <abdulsamadsiddiqui2000@gmail.com>
2025-01-23 15:57:12 +0500

    day 5 added

--- src/components/OffersSection.tsx
+++ src/components/OffersSection.tsx
@@ -4,11 +4,11 @@ import CustomOffers from "./CustomOffers";
 import { client } from "@/sanity/lib/client";
 import { productsQuery } from "@/sanity/lib/queries";
 import { Product } from "@/types/products";
+import Loader from "./Loader";
 
 const OffersSection = () => {
   const [newProducts, setNewProducts] = useState<Product[] | null>(null);
   const [topSelling, setTopSelling] = useState<Product[] | null>(null);
-  const [isOnline, setIsOnline] = useState<boolean>(navigator.onLine);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
@@ -29,28 +29,6 @@ const OffersSection = () => {
     fetchData();
   }, []);
 
-  useEffect(() => {
-    const updateNetworkStatus = () => {
-      setIsOnline(navigator.onLine);
-    };
-
-    window.addEventListener("online", updateNetworkStatus);
-    window.addEventListener("offline", updateNetworkStatus);
-
-    return () => {
-      window.removeEventListener("online", updateNetworkStatus);
-      window.removeEventListener("offline", updateNetworkStatus);
-    };
-  }, []);
-
-  if (!isOnline) {
-    return (
-      <h1 className="text-4xl text-center font-bold my-16 text-red-500">
-        You are offline. Please check your network connection.
-      </h1>
-    );
-  }
-
   if (error) {
     return (
       <h1 className="text-4xl text-center font-bold my-16 text-red-500">
@@ -60,11 +38,7 @@ const OffersSection = () => {
   }
 
   if (!newProducts || !topSelling) {
-    return (
-      <h1 className="text-4xl text-center font-bold my-16 text-red-500">
-        Loading products...
-      </h1>
-    );
+    return <Loader />;
   }
 
   return (
